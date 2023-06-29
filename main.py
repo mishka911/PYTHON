90c1e58f18b049b85d101cbc0ec2325b4503cd6f
Vanessa Proaño <125935324+mishka911@users.noreply.github.com>
2023-06-29 01:33:33 -0500

    Generador de nombres de banda con la ciudad y nombre de mascota

---
+++ main.py
@@ -0,0 +1,4 @@
+print("bienvenido al generador de nombres de bandas.")
+c = input("nombre de la ciudad donde creciste?\n")
+p = input("cual es el nombre de tu mascota?\n")
+print("el nombre de tu banada puede ser " + c + " " + p)
